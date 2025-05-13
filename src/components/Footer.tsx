e25a9be1917c7cff1e910e10b50dbbf4dcf2c1ab
Rahul Jha <140407586+Rahul251202@users.noreply.github.com>
2025-05-14 04:46:43 +0530

    product add

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -31,10 +31,10 @@ const Footer = () => {
           <RiSecurePaymentLine />
           <p>Swift Shipping</p>
         </div>
-        <div className="utility-item">
+        {/* <div className="utility-item">
           <RiSecurePaymentLine />
           <p>Money Return</p>
-        </div>
+        </div> */}
       </div>
       <footer className="site-footer">
       <div className="footer-container">
